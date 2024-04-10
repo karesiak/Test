d5955375ac4e486ffeb0535cc5bf433c5e578905
karesiak <kordek0945@gmail.com>
2024-04-10 11:42:56 +0200

    Add project files.

---
+++ Test/Controllers/UczenController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test.Models;
+using System.Linq;
+
+namespace Test.Controllers
+{
+    public class UczenController : Controller
+    {
+        private static readonly List<Uczen> uczenList = new List<Uczen>(); // Lista uczniów
+
+        public IActionResult Form()
+        {
+            return View(new Uczen());
+        }
+
+        [HttpPost]
+        public IActionResult Form(Uczen uczen)
+        {
+            if (ModelState.IsValid)
+            {
+                if (uczenList.Any(u => u.Id == uczen.Id))
+                {
+                    ModelState.AddModelError("Id", "Student z takim ID już istnieje.");
+                    return View(uczen);
+                }
+
+                uczenList.Add(uczen); // Dodajemy ucznia do listy
+                return RedirectToAction("Index"); // Przekierowanie do widoku z listą
+            }
+            return View(uczen);
+        }
+
+        public IActionResult Index()
+        {
+            return View(uczenList); // Przekazujemy listę uczniów do widoku
+        }
+    }
+}
---
+++ Test/Models/Uczen.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.Models
+{
+    public class Uczen
+    {
+        [Required]
+        [RegularExpression(@"^\d{4}$", ErrorMessage ="Id wymaga 4 znaków")]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Data { get; set; }
+
+        [Required]
+        public int Ocena { get; set; }
+
+        public static readonly string[] TypyOcen = new string[] { "2", "3", "4", "5" };
+       
+       
+    }
+}
